55a2a749783bfc4b80c1b3761dbdac0205310c49
Accel <cyberpunk2076@gmail.com>
2021-05-11 12:09:05 +0700

    first commit

---
+++ src/pages/_app.js
@@ -0,0 +1,68 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "./theme";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    containerBar: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      minHeight: "40px",
+    },
+    title: {},
+  })
+);
+
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+  const classes = useStyles();
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Quản lý bến xe</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <AppBar position="static">
+          <Toolbar className={classes.containerBar}>
+            <Typography variant="h6" className={classes.title}>
+              Bến xe Trung tâm Đà Nẵng
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
